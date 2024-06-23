d05f9c65a02de3d1b2b5aa93ed3c74fbdec8056c
Adam Nagy <aduxik@gmail.com>
2024-06-23 23:05:24 +0200

    added 3 different subjects

--- main.py
+++ main.py
@@ -1,54 +1,49 @@
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, recall_score, precision_score, confusion_matrix
-from sklearn.impute import SimpleImputer
-from sklearn.model_selection import cross_val_score, StratifiedKFold, KFold
+from sklearn.metrics import accuracy_score, recall_score, confusion_matrix
+from sklearn.model_selection import StratifiedKFold
 
 # Load the CSV file
 data = pd.read_csv('students_results_by_year.csv', sep=',')
-# print(data.head())
 
 # Encode the grades
 grade_mapping = {'A': 5, 'B': 4, 'C': 3, 'D': 2, 'E': 1, 'Fx': 0}
-data = data.map(lambda x: grade_mapping[x] if x in grade_mapping else np.nan).dropna(subset=['Algoritmy a údajové štruktúry 2'])
+data = (data.map(lambda x: grade_mapping[x] if x in grade_mapping else np.nan)
+        .dropna(subset=['Algoritmy a údajové štruktúry 2'])
+        .dropna(subset=['Optimalizácia sietí'])
+        .dropna(subset=['Diskrétna simulácia']))
 
+aus2_subjects = ['Algoritmy a údajove štruktúry 1', 'Informatika 3', 'Informatika 2', 'Algoritmy a údajové štruktúry 2']
+opts_subjects = ['Matematika pre informatikov', 'Algoritmická teória grafov', 'Diskrétna optimalizácia', 'Optimalizácia sietí']
+dis_subjects = ['Diskrétna pravdepodobnosť', 'Pravdepodobnosť a štatistika', 'Modelovanie a simulácia', 'Diskrétna simulácia']
 
-subjects_to_keep = ['Algoritmy a údajove štruktúry 1', 'Informatika 3', 'Informatika 2', 'Algoritmy a údajové štruktúry 2']
-data = data[subjects_to_keep]
-aus2 = data['Algoritmy a údajové štruktúry 2']
+dataAUS = data[aus2_subjects].copy()
+dataOPTS = data[opts_subjects].copy()
+dataDIS = data[dis_subjects].copy()
 
 # Define the target variable (pass/fail)
-data['pass_fail'] = data.apply(lambda row: 1 if row['Algoritmy a údajové štruktúry 2'] > 0 else 0, axis=1)
+dataAUS['pass_fail'] = dataAUS.apply(lambda row: 1 if row['Algoritmy a údajové štruktúry 2'] > 0 else 0, axis=1)
+dataOPTS['pass_fail'] = dataOPTS.apply(lambda row: 1 if row['Optimalizácia sietí'] > 0 else 0, axis=1)
+dataDIS['pass_fail'] = dataDIS.apply(lambda row: 1 if row['Diskrétna simulácia'] > 0 else 0, axis=1)
 
 
 # Split the data into features and target
-X = data.drop(columns=['Algoritmy a údajové štruktúry 2', 'pass_fail'])
-y = data['pass_fail']
-print(data)
+X_AUS = dataAUS.drop(columns=['Algoritmy a údajové štruktúry 2', 'pass_fail'])
+y_AUS = dataAUS['pass_fail']
+X_OPTS = dataOPTS.drop(columns=['Optimalizácia sietí', 'pass_fail'])
+y_OPTS = dataOPTS['pass_fail']
+X_DIS = dataDIS.drop(columns=['Diskrétna simulácia', 'pass_fail'])
+y_DIS = dataDIS['pass_fail']
 
+X_all = [X_AUS, X_OPTS, X_DIS]
+y_all = [y_AUS, y_OPTS, y_DIS]
 
 model = LogisticRegression()
-# model.fit(X, y)
-
-# Evaluate the model
-# y_pred = model.predict(X_test)
-# accuracy = accuracy_score(y_test, y_pred)
-# conf_matrix = confusion_matrix(y_test, y_pred)
-
-# print(f'Accuracy: {accuracy}')
-# print(f'Confusion Matrix:\n{conf_matrix}')
-
-# scores = cross_val_score(model, X, y, cv=5, scoring='accuracy')
-
-# print(f'Cross-validation scores: {scores}')
-# print(f'Mean cross-validation score: {scores.mean()}')
-
 
 # Initialize Stratified K-Fold
 skf = StratifiedKFold(n_splits=5)
-kf = KFold(n_splits=5)
+
 # Define the threshold values
 thresholds = np.arange(0.1, 1.0, 0.1)
 
@@ -65,58 +60,63 @@ def specificity_score(y_true, y_pred):
     return tn / (tn + fp)
 
 
-# Loop over thresholds
-for threshold in thresholds:
-    train_accuracies = []
-    train_specificities = []
-    train_recalls = []
-    train_means = []
-    val_accuracies = []
-    val_specificities = []
-    val_recalls = []
-    val_means = []
+for i in range(3):
+    X = X_all[i]
+    y = y_all[i]
 
-    # Perform Stratified K-Fold cross-validation
-    for train_index, val_index in skf.split(X, y):
-        X_train, X_val = X.iloc[train_index], X.iloc[val_index]
-        y_train, y_val = y.iloc[train_index], y.iloc[val_index]
+    # Loop over thresholds
+    for threshold in thresholds:
+        train_accuracies = []
+        train_specificities = []
+        train_recalls = []
+        train_means = []
+        val_accuracies = []
+        val_specificities = []
+        val_recalls = []
+        val_means = []
 
-        # Train the model
-        model.fit(X_train, y_train)
+        # Perform Stratified K-Fold cross-validation
+        for train_index, val_index in skf.split(X, y):
+            X_train, X_val = X.iloc[train_index], X.iloc[val_index]
+            y_train, y_val = y.iloc[train_index], y.iloc[val_index]
 
-        # Predict probabilities
-        y_train_pred_prob = model.predict_proba(X_train)[:, 1]
-        y_val_pred_prob = model.predict_proba(X_val)[:, 1]
+            # Train the model
+            model.fit(X_train, y_train)
 
-        # Apply threshold
-        y_train_pred = (y_train_pred_prob >= threshold).astype(int)
-        y_val_pred = (y_val_pred_prob >= threshold).astype(int)
+            # Predict probabilities
+            y_train_pred_prob = model.predict_proba(X_train)[:, 1]
+            y_val_pred_prob = model.predict_proba(X_val)[:, 1]
 
-        # Calculate metrics for train
-        train_accuracies.append(accuracy_score(y_train, y_train_pred))
-        train_specificities.append(specificity_score(y_train, y_train_pred))
-        train_recalls.append(recall_score(y_train, y_train_pred))
-        train_means.append((specificity_score(y_train, y_train_pred) + recall_score(y_train, y_train_pred)) / 2)
+            # Apply threshold
+            y_train_pred = (y_train_pred_prob >= threshold).astype(int)
+            y_val_pred = (y_val_pred_prob >= threshold).astype(int)
 
-        # Calculate metrics for validation
-        val_accuracies.append(accuracy_score(y_val, y_val_pred))
-        val_specificities.append(specificity_score(y_val, y_val_pred))
-        val_recalls.append(recall_score(y_val, y_val_pred))
-        val_means.append((specificity_score(y_val, y_val_pred) + recall_score(y_val, y_val_pred)) / 2)
+            # Calculate metrics for train
+            train_accuracies.append(accuracy_score(y_train, y_train_pred))
+            train_specificities.append(specificity_score(y_train, y_train_pred))
+            train_recalls.append(recall_score(y_train, y_train_pred))
+            train_means.append((specificity_score(y_train, y_train_pred) + recall_score(y_train, y_train_pred)) / 2)
 
-    # Append results to the dataframe
-    results = results._append({
-        't': threshold,
-        'T_Total': np.mean(train_accuracies),
-        'T_Specificita': np.mean(train_specificities),
-        'T_Senzitivita': np.mean(train_recalls),
-        'T_Priemer': np.mean(train_means),
-        'V_Total': np.mean(val_accuracies),
-        'V_Specificita': np.mean(val_specificities),
-        'V_Senzitivita': np.mean(val_recalls),
-        'V_Priemer': np.mean(val_means)
-    }, ignore_index=True)
+            # Calculate metrics for validation
+            val_accuracies.append(accuracy_score(y_val, y_val_pred))
+            val_specificities.append(specificity_score(y_val, y_val_pred))
+            val_recalls.append(recall_score(y_val, y_val_pred))
+            val_means.append((specificity_score(y_val, y_val_pred) + recall_score(y_val, y_val_pred)) / 2)
 
-# Display the results
-print(results.all)
-results.to_csv('results.csv', index=False)
+        # Append results to the dataframe
+        results = results._append({
+            't': threshold,
+            'T_Total': np.mean(train_accuracies),
+            'T_Specificita': np.mean(train_specificities),
+            'T_Senzitivita': np.mean(train_recalls),
+            'T_Priemer': np.mean(train_means),
+            'V_Total': np.mean(val_accuracies),
+            'V_Specificita': np.mean(val_specificities),
+            'V_Senzitivita': np.mean(val_recalls),
+            'V_Priemer': np.mean(val_means)
+        }, ignore_index=True)
+
+    results.to_csv(f'results_{i}.csv', index=False)
+    results = pd.DataFrame(
+    columns=['t', 'T_Total', 'T_Specificita', 'T_Senzitivita', 'T_Priemer', 'V_Total',
+             'V_Specificita', 'V_Senzitivita', 'V_Priemer'])
